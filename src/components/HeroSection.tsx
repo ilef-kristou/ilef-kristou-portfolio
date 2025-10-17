84b7d6b53c2834a8bff2713f5ba00977d169a2cc
ilefkristou992@gmail.com <ilefkristou992@gmail.com>
2025-10-17 17:01:14 +0100

    modification du CV

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -51,7 +51,7 @@ const HeroSection = () => {
                 <ArrowRight className="ml-2 group-hover:translate-x-1 transition-transform" />
               </Button>
               
-              <a href="CV-ilef.pdf" download="CV-ilef-kristou.pdf">
+              <a href="CV.pdf" download="CV-ilef-kristou.pdf">
                 <Button
                   variant="outline"
                   size="lg"
